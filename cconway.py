35438231b73efaee0e9c88b30529c050a28bc1ad
WodkaRHR <domi.erdnuss2@gmx.de>
2017-07-12 23:18:54 +0200

    presentation

--- cconway.py
+++ cconway.py
@@ -5,8 +5,6 @@ A = 10
 P = 4
 _B = [3]
 _D = [2, 3]
-<<<<<<< HEAD
-=======
 DISTANCE = "euclid"
 _distances = {
     "euclid" : (lambda x0, y0, x1, y1: math.sqrt(math.pow(x0 - x1, 2) + math.pow(y0 - y1, 2))),
@@ -14,7 +12,6 @@ _distances = {
     "max" : (lambda x0, y0, x1, y1: max(abs(x0 - x1), abs(y0 - y1))) 
 }
 
->>>>>>> 42bca687494647403ca3e87d2b452d881be3620d
 
 def polynomial_functor(roots):
     """ Functor to create a pol. function a*(1-(1/(1 + (x-ri))))"""
@@ -25,11 +22,7 @@ def polynomial_functor(roots):
         return r
     return f
 
-<<<<<<< HEAD
-1   def _b(f):
-=======
 def _b(f):
->>>>>>> 42bca687494647403ca3e87d2b452d881be3620d
     """ Best birth rate at field = 3 """
     return polynomial_functor(_B)(f)
 
@@ -37,32 +30,22 @@ def _d(f):
     """ Cell survives best at 2 or 3 neighbours """
     return polynomial_functor(_D)(f)
 
+
+
 class CGrid:
     """ Continuous grid"""
-<<<<<<< HEAD
-    def __init__(self, width, height, b=_b, d=_d, f=None, xrange=2, yrange=2):
-        """ b(f) (birth rate) and d(f) (durability function) are polynomial
-        functions depending on the strength of the surrounding field
-        xrange, yrange defines how many neighbours are creating a field for a cell"""
-        if not f: f = self._field
-        self.width = width
-        self.height = height
-        self.reset()
-        self.f = f #default field function
-=======
-    def __init__(self, width, height, b=_b, d=_d, xrange=2, yrange=2):
+    def __init__(self, width, height, b=_b, d=_d, xrange=2, yrange=2, borders_connected=True):
         """ b(f) (birth rate) and d(f) (durability function) are polynomial
         functions depending on the strength of the surrounding field
         xrange, yrange defines how many neighbours are creating a field for a cell"""
         self.width = width
         self.height = height
         self.reset()
->>>>>>> 42bca687494647403ca3e87d2b452d881be3620d
         self.b = b
         self.d = d
         self.xrange = xrange
         self.yrange = yrange
-        self.CONST = 1 / (xrange + yrange) 
+        self.borders_connected = borders_connected
 
     def reset(self):
         """ Resets the cells to 0.0 """
@@ -74,12 +57,7 @@ class CGrid:
 
     def _distance(self, x0, y0, x1, y1):
         """ Calculates the distance between two points"""
-<<<<<<< HEAD
-        dx, dy = x1 - x0, y1 - y0
-        return math.sqrt(dx*dx + dy*dy)
-=======
         return _distances[DISTANCE](x0, y0, x1, y1)
->>>>>>> 42bca687494647403ca3e87d2b452d881be3620d
 
 
     def _iter_field(self):
@@ -91,9 +69,8 @@ class CGrid:
             x1 = x - dx
             y1 = y - dy
             if dx == 0 and dy == 0: continue #Cell does not induce its own field
-            if x1 in range(self.width) and y1 in range(self.height):
-                d = self._distance(x, y, x1, y1)
-                self.field[y][x] += self.cells[y1][x1] / math.pow(d, 2)# * self.CONST
+            d = self._distance(x, y, x1, y1)
+            self.field[y][x] += self.get_cell(x1, y1) / math.pow(d, 2)# * self.CONST
 
     def print_field(self):
         print(self.field)
@@ -108,22 +85,28 @@ class CGrid:
         global P
         P = p
 
+    def get_cell(self, x, y):
+        if self.borders_connected:
+            x = x % self.width
+            y = y % self.height
+        if x in range(self.width) and y in range(self.height):
+            return self.cells[y][x]
+        return 0
+
     def iter_cells(self):
         """ Iterate all cells """
         self._iter_field()
         for y in range(self.height):
             for x in range(self.width):
-                c_last = self.cells[y][x]
+                c_last = self.get_cell(x, y)
                 f = self.field[y][x]
                 self.cells[y][x] = 1 / (1 + self.b(f) * math.pow(1 - c_last, 1) + self.d(f) * math.pow(c_last, 1))
 
-<<<<<<< HEAD
-=======
+
     def set_DISTANCE(self, distance):
         """ Changes the distance method """
         global DISTANCE
         DISTANCE = distance
 
->>>>>>> 42bca687494647403ca3e87d2b452d881be3620d
 
         
--- gui.py
+++ gui.py
@@ -4,15 +4,13 @@ import PIL.ImageTk as ImageTk
 import cconway
 import numpy as np
 import tkinter.messagebox
-<<<<<<< HEAD
-=======
 import _thread
 import time
->>>>>>> 42bca687494647403ca3e87d2b452d881be3620d
+import tkinter.simpledialog
 
 #Only define integer values
-WIDTH, HEIGHT = 100, 100
-ZOOM = 6
+WIDTH, HEIGHT = 50, 50
+ZOOM = 12
 
 
 class Gui(Tk.Frame):
@@ -21,15 +19,13 @@ class Gui(Tk.Frame):
         self.root = root
         super().__init__(master=root)
         self.grid = cconway.CGrid(WIDTH, HEIGHT)
-<<<<<<< HEAD
-=======
         self.auto_iterate_enable = False
         self.auto_iterate_running = False
->>>>>>> 42bca687494647403ca3e87d2b452d881be3620d
         canvas_frame = Tk.Frame(master=root)
         canvas_frame.grid(row=0, column=0)
         self.canvas = Tk.Canvas(canvas_frame, width=WIDTH * ZOOM, height = HEIGHT * ZOOM)
         self.canvas.pack()
+        self.canvas_image = None
 
         ui_frame = Tk.Frame(master=root)
         ui_frame.grid(row=0, column=1)
@@ -54,29 +50,32 @@ class Gui(Tk.Frame):
 
         Tk.Label(master=ui_frame, text="Durability function roots").grid(row=6, sticky=Tk.NW)
         self.entry_durability_function_roots = Tk.Entry(master=ui_frame)
-<<<<<<< HEAD
-        self.entry_durability_function_roots.grid(row=5, sticky=Tk.NW)
-        self.entry_durability_function_roots.insert(0, ",".join(map(str, cconway._D)))
-        self.entry_durability_function_roots.bind("<Return>", lambda e: self.set_durability_function_roots())
-
-        Tk.Button(master=ui_frame, command=self.iter, text="Iterate").grid(row=8, sticky=Tk.NW)
-        Tk.Button(master=ui_frame, command=self.random, text="Random").grid(row=9, sticky=Tk.NW)
-        Tk.Button(master=ui_frame, command=self.reset, text="Reset").grid(row=10, sticky=Tk.NW)
-        self.show()
-        self.canvas.bind("<ButtonPress-1>", lambda e: self.set(int(e.x / ZOOM), int(e.y / ZOOM), 1.0))
-        self.canvas.bind("<ButtonPress-3>", lambda e: self.set(int(e.x / ZOOM), int(e.y / ZOOM), 0.0))
-=======
         self.entry_durability_function_roots.grid(row=7, sticky=Tk.NW)
         self.entry_durability_function_roots.insert(0, ",".join(map(str, cconway._D)))
         self.entry_durability_function_roots.bind("<Return>", lambda e: self.set_durability_function_roots())
 
-        Tk.Label(master=ui_frame, text="Distance metric").grid(row=8, sticky=tkinter.NW)
-        self.distance_metric = Tk.StringVar(master=ui_frame, value="euclid")
-        Tk.OptionMenu(ui_frame, self.distance_metric, "euclid", "max", "average").grid(row=9, sticky=tkinter.NW)
+        
+        Tk.Label(master=ui_frame, text="Field computation range").grid(row=8, sticky=Tk.NW)
+        self.entry_field_computation_range = Tk.Entry(master=ui_frame)
+        self.entry_field_computation_range.grid(row=9, sticky=Tk.NW)
+        self.entry_field_computation_range.insert(0, ",".join(map(str, [self.grid.xrange, self.grid.yrange])))
+        self.entry_field_computation_range.bind("<Return>", lambda e: self.set_field_computation_range())
 
-        Tk.Button(master=ui_frame, command=self.iter, text="Iterate").grid(row=10, sticky=Tk.NW)
-        Tk.Button(master=ui_frame, command=self.random, text="Random").grid(row=11, sticky=Tk.NW)
-        Tk.Button(master=ui_frame, command=self.reset, text="Reset").grid(row=12, sticky=Tk.NW)
+        self.borders_connected = Tk.IntVar(master=ui_frame, value=int(self.grid.borders_connected))
+        self.checkbox_borders_connected = Tk.Checkbutton(master=ui_frame, text="Borders topologically connected", var=self.borders_connected, command=self.set_borders_connected)
+        self.checkbox_borders_connected.grid(row=10, sticky=tkinter.NW)
+
+        Tk.Label(master=ui_frame, text="Distance metric").grid(row=11, sticky=tkinter.NW)
+        self.distance_metric = Tk.StringVar(master=ui_frame, value="euclid")
+        self.option_menu_distance_metric = Tk.OptionMenu(ui_frame, self.distance_metric, "euclid", "max", "average")
+        self.option_menu_distance_metric.grid(row=12, sticky=tkinter.NW)
+
+        self.button_iterate = Tk.Button(master=ui_frame, command=self.iter, text="Iterate")
+        self.button_iterate.grid(row=13, sticky=Tk.NW)
+        self.button_random = Tk.Button(master=ui_frame, command=self.random, text="Random")
+        self.button_random.grid(row=14, sticky=Tk.NW)
+        self.button_reset = Tk.Button(master=ui_frame, command=self.reset, text="Reset")
+        self.button_reset.grid(row=15, sticky=Tk.NW)
         self.canvas.bind("<ButtonPress-1>", lambda e: self.set(int(e.x / ZOOM), int(e.y / ZOOM), 1.0))
         self.canvas.bind("<ButtonPress-3>", lambda e: self.set(int(e.x / ZOOM), int(e.y / ZOOM), 0.0))
         def button_auto_iterate_pressed():
@@ -88,10 +87,12 @@ class Gui(Tk.Frame):
                 
 
         self.button_auto_iterate = Tk.Button(master=ui_frame, command=button_auto_iterate_pressed, text="Auto iterate")
-        self.button_auto_iterate.grid(row=13, sticky=Tk.NW)
+        self.button_auto_iterate.grid(row=16, sticky=Tk.NW)
         self.show()
+
+        self.button_export = Tk.Button(master=ui_frame, command=self.export, text="Export")
+        self.button_export.grid(row=17, sticky=Tk.NW)
                 
->>>>>>> 42bca687494647403ca3e87d2b452d881be3620d
 
 
     def parse_roots(self, s):
@@ -122,6 +123,15 @@ class Gui(Tk.Frame):
             tkinter.messagebox.showerror("Unable to define durability function (format = 'x1,x2....,xn)'", "During definition of the durability function an exception occured: " + str(e))
             return False
 
+    def set_borders_connected(self):
+        """ Sets if the borders are topologically connected """
+        try:
+            self.grid.borders_connected = bool(self.borders_connected.get())
+            return True
+        except Exception as e:
+            tkinter.messagebox.showerror("Could not set the connection of the borders", "Following exception occured: " + str(e))
+            return False
+
 
     def random(self):
         """ Randomizes the entire grid """
@@ -135,6 +145,7 @@ class Gui(Tk.Frame):
 
     def set(self, x, y, v):
         """ Sets a cell to a given value"""
+        if self.auto_iterate_running: return
         self.grid.cells[y][x] = v
         self.show()
 
@@ -156,8 +167,6 @@ class Gui(Tk.Frame):
             tkinter.messagebox.showerror("Invalid flattness", "Invalid flattness: Float expected!")
             return False
 
-<<<<<<< HEAD
-=======
     def set_distance_metric(self):
         """ Sets the distance metric used """
         try:
@@ -165,22 +174,29 @@ class Gui(Tk.Frame):
             return True
         except:
             tkinter.messagebox.showerror("Invalid distance metric", "Invalid distance metric!")
-            return False    
+            return False 
 
->>>>>>> 42bca687494647403ca3e87d2b452d881be3620d
+    def set_field_computation_range(self):
+        """ Sets the computation range of the field function """
+        try:
+            x, y = map(int, self.entry_field_computation_range.get().split(","))
+            self.grid.xrange = x
+            self.grid.yrange = y
+            return True
+        except:
+            tkinter.messagebox.showerror("Invalid field computation range", "Invalid field computation range, expected x,y as integers!")
+            return False  
+
 
     def show(self):
         grey = np.array([[int((1-c) * 255) for c in line ] for line in self.grid.cells])
         self.im = Image.fromarray(grey).resize((WIDTH * ZOOM, HEIGHT * ZOOM))
         self.pi = ImageTk.PhotoImage(self.im)
-        self.canvas.create_image(1, 1, image=self.pi, anchor=Tk.NW)
+        if self.canvas_image: self.canvas.delete(self.canvas_image)
+        self.canvas_image = self.canvas.create_image(1, 1, image=self.pi, anchor=Tk.NW)
     
     def iter(self):
-<<<<<<< HEAD
-        if self.set_spectrum() and self.set_flattness() and self.set_birth_function_roots() and self.set_durability_function_roots():
-=======
-        if self.set_spectrum() and self.set_flattness() and self.set_birth_function_roots() and self.set_durability_function_roots() and self.set_distance_metric():
->>>>>>> 42bca687494647403ca3e87d2b452d881be3620d
+        if self.set_spectrum() and self.set_flattness() and self.set_birth_function_roots() and self.set_durability_function_roots() and self.set_distance_metric() and self.set_field_computation_range() and self.set_borders_connected():
             try: 
                 self.grid.iter_cells()
                 self.show()
@@ -190,21 +206,43 @@ class Gui(Tk.Frame):
             
         #self.grid.print_field()
 
-<<<<<<< HEAD
-=======
     def auto_iterate(self):
         """ Function for auto iterating (called inside a paralell thread)"""
         self.button_auto_iterate["text"] = "Stop"
         self.auto_iterate_running = True
+        widgets_to_disable = [
+            self.entry_birth_function_roots, self.entry_durability_function_roots, self.entry_field_computation_range, self.entry_flattness, self.entry_spectrum,
+            self.button_iterate, self.button_random, self.button_reset, self.option_menu_distance_metric, self.checkbox_borders_connected
+            ]
+        for widget in widgets_to_disable:
+            widget.configure(state = "disabled")
         while self.auto_iterate_enable:
+            t0 = time.time()
             self.iter()
-            time.sleep(0.5)
+            t1 = time.time()
+            dt = 0.5 - (t1 - t0)
+            if dt > 0: time.sleep(dt)
+        for widget in widgets_to_disable:
+            widget.configure(state = "normal")
         self.button_auto_iterate["text"] = "Auto iterate"
         self.auto_iterate_running = False
 
->>>>>>> 42bca687494647403ca3e87d2b452d881be3620d
+    def export(self):
+        """ Exports the next iterations """
+        num = Tk.simpledialog.askinteger("Number of frames to export", "Enter number of frames to export")
+        if not num: return
+        path = Tk.simpledialog.askstring("Enter basename", "Enter basename of frames to export (e.g. 'foo/bar' provides foo/bar0.png, foo/bar1.png...)")
+        if not path: return
+        for i in range(num):
+            grey = np.array([[int((1-c) * 255) for c in line ] for line in self.grid.cells])
+            image = Image.new("RGB", (WIDTH * ZOOM, HEIGHT * ZOOM))
+            image.paste(Image.fromarray(grey).resize((WIDTH * ZOOM, HEIGHT * ZOOM)))
+            image.save(path + str(i) + ".png", "PNG")
+            self.iter()
+
 
 root = Tk.Tk()
+root.wm_title("Continous Conway's Game Of Life")
 gui = Gui(root)
 root.mainloop()
 
